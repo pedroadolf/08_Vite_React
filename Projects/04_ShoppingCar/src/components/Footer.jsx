8324f3facfeef651f6233e8489740c90b40b8651
pedroadolf <62665196+pedroadolf@users.noreply.github.com>
2024-05-16 10:53:10 -0600

    add project 4

---
+++ Projects/04_ShoppingCar/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export function Footer() {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className="footer">
+      <h4>
+        Prueba  React ⚛️ － <span>@pash</span>
+      </h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
+    </footer>
+  );
+}
